8b337f97d371bac3146b188236e65cf2decb6bdb
Jonathan Rodrigues <jonathan.a.rodr@gmail.com>
2021-10-13 10:41:44 -0300

    Add bottom navigation

--- sample/src/main/kotlin/br/com/inclusion/view/MainActivity.kt
+++ sample/src/main/kotlin/br/com/inclusion/view/MainActivity.kt
@@ -2,11 +2,25 @@ package br.com.inclusion.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import br.com.inclusion.R
+import br.com.inclusion.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setupNavigation()
+    }
+
+    private fun setupNavigation() {
+        findNavController(R.id.nav_host_fragment).apply {
+            binding.bottomNavigation.setupWithNavController(this)
+        }
     }
 }
